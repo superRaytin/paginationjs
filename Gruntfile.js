2a37763213a5f17dd3ab51c6068b41fb97cf6f42
leon.shi <shiliang.sl@alibaba-inc.com>
2015-10-28 15:00:31 +0800

    gruntfile adjustment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,69 @@
-﻿// grunt
+﻿
 module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         uglify: {
             options: {
                 banner: '/*\n' +
                     ' * pagination.js <%= pkg.version %>\n' +
                     ' * <%= pkg.description %>\n' +
-                    ' * https://github.com/superRaytin/paginationjs\n\n' +
-                    ' * Homepage: http://paginationjs.com\n' +
+                    ' * <%= pkg.repository.url %>\n\n' +
+                    ' * Homepage: <%= pkg.homepage %>\n' +
                     ' *\n' +
-                    ' * Copyright 2014-2100, superRaytin\n' +
-                    ' * Released under the MIT license.\n' +
+                    ' * Copyright 2014-2100, <%= pkg.author %>\n' +
+                    ' * Released under the <%= pkg.license %> license.\n' +
                     '*/\n'
             },
             main: {
                 files: [
                     {
-                        src: ['dist/pagination.js'],
+                        src: ['src/pagination.js'],
                         dest: 'dist/pagination.min.js'
-                    },
-                    {
-                        src: ['dist/pagination-with-styles.js'],
-                        dest: 'dist/pagination-with-styles.min.js'
                     }
                 ]
             }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    {
+                        src: 'src/pagination.js',
+                        dest: 'dist/pagination.js'
+                    },
+                    {
+                        src: 'src/pagination.less',
+                        dest: 'dist/pagination.less'
+                    }
+                ]
+            }
+        },
+
+        less: {
+            main: {
+                src: 'src/pagination.less',
+                dest: 'dist/pagination.css'
+            }
+        },
+
+        cssmin: {
+            main: {
+                src: 'dist/pagination.css',
+                dest: 'dist/pagination.css'
+            }
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify']);
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', [
+        'uglify',
+        'copy',
+        'less',
+        'cssmin'
+    ]);
 };
