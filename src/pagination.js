84654e282c537305631ad0114050fb2b5c3154ae
Leon Shi <superRaytin@gmail.com>
2017-11-02 13:28:29 +0800

    bump version 2.2.0

--- src/pagination.js
+++ src/pagination.js
@@ -1,14 +1,3 @@
-/*
- * pagination.js 2.1.0
- * A jQuery plugin to provide simple yet fully customisable pagination.
- * https://github.com/superRaytin/paginationjs
- *
- * Homepage: http://pagination.js.org
- *
- * Copyright 2014-2100, superRaytin
- * Released under the MIT license.
- */
-
 (function(global, $) {
 
   if (typeof $ === 'undefined') {
